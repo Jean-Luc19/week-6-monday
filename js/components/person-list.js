287427294565bd81648bf11fd99dc8ed98a282c0
Jean-Luc19 <aticus19@gmail.com>
2017-03-05 13:31:33 -0700

    adds props to person component

--- js/components/person-list.js
+++ js/components/person-list.js
@@ -2,13 +2,16 @@ import React from 'react';
 import Person from './person';
 
 export default function PersonList() {
-  const people = [];
-  for (let i = 0; i < 25; i++) {
-    people.push(<Person />);
-  }
-  return (
-    <div className="person-list">
-      {people}
-    </div>
-  );
+  Person.defaultProps = {
+    imageUrl: 'http://www.gravatar.com/avatar/?d=identicon',
+    job: 'Entertaining Er'
+};
+    return (
+        <div className="person-list">
+            <Person name="Derek Zoolander" />
+            <Person name="Donald Knuth"
+                    imageUrl="http://www-cs-faculty.stanford.edu/~uno/don.gif"
+                    job="Clever chap" />
+        </div>
+    );
 }
--- js/components/person.js
+++ js/components/person.js
@@ -1,14 +1,11 @@
 import React from 'react';
 
-export default function Person() {
-  const name = 'Derek Zoolander';
-  const imageUrl = 'http://media.architecturaldigest.com/photos/56be596a202b83b31f121405/master/pass/zoolander-2-movie-set-design-001.jpg';
-  const job = 'Male Model';
+export default function Person(props) {
   return (
     <div className="person">
-        <div className ="person-name">{name}</div>
-        <img className = "person-img" src={imageUrl} />
-        <div className = "person-job">{job}</div>
+        <div className ="person-name">{props.name}</div>
+        <img className = "person-img" src={props.imageUrl} />
+        <div className = "person-job">{props.job}</div>
     </div>
   );
 }
